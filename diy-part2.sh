32dc6eda54396e9db129076977d22a0dfb44ffae
Fallen Meteor <32792932+FallenMe@users.noreply.github.com>
2022-06-21 19:46:22 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,4 +13,5 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+git clone https://github.com/fw876/helloworld.git package/helloworld
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
